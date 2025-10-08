1d58f99eb026a16d8adeaefc868bf7b60fdbf62e
Adarsh Srivastav <adarshsrivastav49455@gmail.com>
2025-10-08 19:24:11 +0530

    add validAnagram

---
+++ validAnagram.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+        if(s.length() != t.length()) return false;
+
+        int freq[26] = {0};
+        for(int i=0;i<s.length();i++){
+            freq[s[i]-'a']++;
+            freq[t[i]-'a']--;
+        }
+
+        for(int i=0;i<26;i++){
+            if(freq[i] != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+int main()
+{
+    string s,t;
+    cin >> s;
+    cin >> t;
+    Solution sol;
+    bool result = sol.isAnagram(s,t);
+    cout << (result ? "true" : "false") << endl;
+}
