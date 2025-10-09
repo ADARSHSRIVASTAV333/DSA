c8b65af4087a935f463281cc9b46d8fbc0ef7a2d
Adarsh Srivastav <adarshsrivastav49455@gmail.com>
2025-10-09 21:37:36 +0530

    added more solution

--- containsDuplicate.cpp
+++ containsDuplicate.cpp
@@ -15,6 +15,26 @@ class Solution{
        }
 };
 
+
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+
+//         unordered_map<int,int> mpp;         
+//         for(int i=0;i<nums.size();i++){
+//             mpp[nums[i]]++;
+//         }
+
+//         for(int i=0;i<nums.size();i++){
+//             if(mpp[nums[i]] > 1){
+//                 return true;;
+//             }
+//         }
+//         return false;
+        
+//     }
+// };
+
 int main()
 {
     int n;
