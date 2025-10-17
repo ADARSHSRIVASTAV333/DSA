18278608b4fb347cbb3542744fef3aa6472405c0
Adarsh Srivastav <adarshsrivastav49455@gmail.com>
2025-10-17 21:00:05 +0530

    add findthedifference

---
+++ findTheDifference.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+        char findTheDifference(string s, string t){
+            int freq[26] = {0};
+            
+            for(int i=0;i<s.length();i++){
+                freq[s[i]-'a']++;
+            }
+            
+            for(int i=0;i<t.length();i++){
+                freq[t[i]-'a']--;
+            }
+            
+            for(int i=0;i<26;i++){
+                if(freq[i] != 0){
+                    return 'a'+i;
+                }
+            }
+            return 0;
+        }
+};
+
+int main()
+{
+    string s,t;
+    cin >> s;
+    cin >> t;
+    Solution obj;
+    char result = obj.findTheDifference(s,t);
+    cout << result << endl;
+}
