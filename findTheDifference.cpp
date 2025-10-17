c7a8435c12ea1efb12480116b9ca6cb839ca54e4
Adarsh Srivastav <adarshsrivastav49455@gmail.com>
2025-10-17 21:57:13 +0530

    add optimized solution

--- findTheDifference.cpp
+++ findTheDifference.cpp
@@ -1,26 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Solution{
-    public:
-        char findTheDifference(string s, string t){
-            int freq[26] = {0};
+// class Solution{
+//     public:
+//         char findTheDifference(string s, string t){
+//             int freq[26] = {0};
             
-            for(int i=0;i<s.length();i++){
-                freq[s[i]-'a']++;
-            }
+//             for(int i=0;i<s.length();i++){
+//                 freq[s[i]-'a']++;
+//             }
             
-            for(int i=0;i<t.length();i++){
-                freq[t[i]-'a']--;
-            }
+//             for(int i=0;i<t.length();i++){
+//                 freq[t[i]-'a']--;
+//             }
             
-            for(int i=0;i<26;i++){
-                if(freq[i] != 0){
-                    return 'a'+i;
-                }
-            }
-            return 0;
-        }
+//             for(int i=0;i<26;i++){
+//                 if(freq[i] != 0){
+//                     return 'a'+i;
+//                 }
+//             }
+//             return 0;
+//         }
+// };
+
+class Solution {
+public:
+    char findTheDifference(string s, string t){
+         char res = 0;
+         for(char c : s) res ^= c;
+         for(char c : t) res ^= c;
+         return res;
+
+    }
 };
 
 int main()
