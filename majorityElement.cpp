003167d3d8bd6ef1181c414cf1ee3128435db712
Adarsh Srivastav <adarshsrivastav49455@gmail.com>
2025-10-26 20:01:09 +0530

    add hashmap method

--- majorityElement.cpp
+++ majorityElement.cpp
@@ -4,24 +4,47 @@ using namespace std;
 // Brute Force Approach
 // Time Complexity : O(n^2)
 // Space Complexity : O(1)
-class Solution{
-    public:
-        int majorityElement(vector<int> &nums){
+
+// class Solution{
+//     public:
+//         int majorityElement(vector<int> &nums){
             
-            int n = nums.size();
-            for(int i=0;i<n;i++){
-                int count = 0;
-                for(int j=0;j<n;j++){
-                    if(nums[i] == nums[j]){
-                        count++;
-                    }
-                }
-                if(count > n/2){
-                    return nums[i];
-                }
+//             int n = nums.size();
+//             for(int i=0;i<n;i++){
+//                 int count = 0;
+//                 for(int j=0;j<n;j++){
+//                     if(nums[i] == nums[j]){
+//                         count++;
+//                     }
+//                 }
+//                 if(count > n/2){
+//                     return nums[i];
+//                 }
+//             }
+//             return -1;
+//         }
+// };
+
+// Using Unordered Map
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+
+        int n = nums.size();
+        unordered_map<int,int> mpp;
+        for(int i=0;i<n;i++){
+            mpp[nums[i]]++;
+
+            if(mpp[nums[i]] > n/2){
+                return nums[i];
             }
-            return -1;
         }
+
+        return -1;
+    }
 };
 
 int main()
