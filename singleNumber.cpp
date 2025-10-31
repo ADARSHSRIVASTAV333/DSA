931649ed8044a23808f08bc38f40b73b61974233
Adarsh Srivastav <adarshsrivastav49455@gmail.com>
2025-11-01 02:06:26 +0530

    add xor approach

--- singleNumber.cpp
+++ singleNumber.cpp
@@ -3,22 +3,35 @@ using namespace std;
 
 // time complexity : O(n)
 // space complexity : O(n)
-class Solution{
-    public:
-        int singleNumber(vector<int> &nums){
-            if(nums.size() == 1) return nums[0];
-            unordered_map<int,int> mpp;
-            for(int i=0;i<nums.size();i++){
-                mpp[nums[i]]++;
-            }
+// class Solution{
+//     public:
+//         int singleNumber(vector<int> &nums){
+//             if(nums.size() == 1) return nums[0];
+//             unordered_map<int,int> mpp;
+//             for(int i=0;i<nums.size();i++){
+//                 mpp[nums[i]]++;
+//             }
             
-            for(int i=0;i<nums.size();i++){
-                if(mpp[nums[i]] == 1){
-                    return nums[i];
-                }
-            }
-            return -1;
+//             for(int i=0;i<nums.size();i++){
+//                 if(mpp[nums[i]] == 1){
+//                     return nums[i];
+//                 }
+//             }
+//             return -1;
+//         }
+// };
+
+// time complexity : O(n)
+// space complexity : O(1)
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int result = 0;
+        for(int num : nums){
+            result ^= num;
         }
+        return result;
+    }
 };
 
 int main()
