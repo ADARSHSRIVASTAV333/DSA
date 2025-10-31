ec7131bda53792c4a152ce14bfdf3fa3a0ca83dc
Adarsh Srivastav <adarshsrivastav49455@gmail.com>
2025-10-31 23:05:37 +0530

    add stack method

--- validParenthesis.cpp
+++ validParenthesis.cpp
@@ -3,28 +3,65 @@ using namespace std;
 
 // time complexity : O(n^2) in worst case
 // space complexity : O(1)
-class Solution{
-    public:
-        bool isValid(string s){
+// class Solution{
+//     public:
+//         bool isValid(string s){
             
-            bool flag = true;
+//             bool flag = true;
             
-            while(flag){
-                flag = false;
+//             while(flag){
+//                 flag = false;
                 
-                if(s.size() < 2)  break;
+//                 if(s.size() < 2)  break;
                 
-                for(int i=0;i<s.size()-1;i++){
-                    if((s[i] == '(' && s[i+1] == ')') || (s[i] == '{' && s[i+1] == '}') || (s[i] == '[' && s[i+1] == ']')){
-                        s.erase(i,2);
-                        flag = true;
-                        break;
-                    }
+//                 for(int i=0;i<s.size()-1;i++){
+//                     if((s[i] == '(' && s[i+1] == ')') || (s[i] == '{' && s[i+1] == '}') || (s[i] == '[' && s[i+1] == ']')){
+//                         s.erase(i,2);
+//                         flag = true;
+//                         break;
+//                     }
+//                 }
+//             }
+//             return s.empty();
+//         }
+// };
+
+// time complexity : O(n)
+// space complexity : O(n)
+class Solution {
+public:
+    bool isValid(string s){
+        stack<char> st;
+
+        for(char c : s){
+
+            if(c == '(') st.push(')');
+            else if(c == '{') st.push('}');
+            else if(c == '[') st.push(']');
+            else{
+                if(st.empty() || st.top() != c){
+                    return false;
+                }
+                else{
+                    st.pop();
                 }
             }
-            return s.empty();
         }
+        return st.empty();
+    }
 };
+
+/*
+------------------ SUMMARY TABLE ------------------
+
+| Situation                 | Example | Detected by                      |
+|----------------------------|----------|----------------------------------|
+| Closing before opening     | ")("     | st.empty() inside loop           |
+| Wrong bracket type         | "([)]"   | st.top() != c inside loop        |
+| Extra opening brackets     | "((("    | return st.empty() at end         |
+
+---------------------------------------------------
+*/
 
 int main()
 {
