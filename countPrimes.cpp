e5fe410bf13837fcc20a2664440301ed7d487508
Adarsh Srivastav <adarshsrivastav49455@gmail.com>
2025-11-07 12:35:12 +0530

    add brute force method

--- countPrimes.cpp
+++ countPrimes.cpp
@@ -1,6 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// Brute Force Approach
+// Time Complexity : O(n√n)
+// Space Complexity : O(1)
+// Time Limit Exceeded for large inputs
+
+class Solution {
+public:
+    bool isPrime(int n){
+        if(n<=1) return false;
+        for(int i=2;i<=sqrt(n);i++){
+            if(n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    int countPrimes(int n) {
+        int count = 0;
+        for(int i=2;i<n;i++){
+            if(isPrime(i)){
+                count++;
+            }
+        }
+        return count;
+    }
+};
+
 // Sieve of Eratosthenes
 // Time Complexity : O(n log log n)
 // Space Complexity : O(n)
