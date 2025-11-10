1b27af79925e5ee1a6c3419a3865d197c37535a5
Adarsh Srivastav <adarshsrivastav49455@gmail.com>
2025-11-10 23:47:04 +0530

    add reverse func method

--- rotateArray.cpp
+++ rotateArray.cpp
@@ -30,6 +30,7 @@ class Solution{
 // Optimal Approach: Reversal Algorithm
 // Time Complexity : O(n-k) + O(k) + O(n) = O(n)
 // Space Complexity : O(1)
+
 class Solution {
 public:
     void reversearr(vector<int> &nums,int start,int end){
@@ -48,6 +49,24 @@ public:
         reversearr(nums,0,n-k-1);
         reversearr(nums,n-k,n-1);
         reversearr(nums,0,n-1);
+    }
+};
+
+// Using STL reverse function
+// Time Complexity : O(n-k) + O(k) + O(n) = O(n)
+// Space Complexity : O(1)
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k){
+        int n = nums.size();
+        if(n == 0) return;
+        k = k % n;
+
+        reverse(nums.begin(),nums.begin() + (n-k));
+        reverse(nums.begin() + (n-k),nums.end());
+        reverse(nums.begin(),nums.end());
+       
     }
 };
 
