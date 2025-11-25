d3dfaea34192944db9fe735defce7e5cc89e3d99
Adarsh Srivastav <adarshsrivastav49455@gmail.com>
2025-11-25 23:45:58 +0530

    add freq arr optimal method

--- sumOfUniqueElements.cpp
+++ sumOfUniqueElements.cpp
@@ -31,6 +31,27 @@ class Solution{
         }
 };
 
+// Optimal Approach (Frequency Array)
+// Time Complexity : O(n)
+// Space Complexity : O(1) -> frequency array of fixed size 101
+
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        int freq[101] = {0};
+        for(int x : nums){
+            freq[x]++;
+        }
+        int sum = 0;
+        for(int i=1;i<=100;i++){
+            if(freq[i] == 1){
+                sum += i;
+            }
+        }
+        return sum;
+    }
+};
+
 int main()
 {
     int n;
