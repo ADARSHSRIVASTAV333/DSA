76a47edfa9709f354de35a73d33e1df0f9a161bf
Adarsh Srivastav <adarshsrivastav49455@gmail.com>
2025-12-05 19:03:44 +0530

    add xor optimized code

--- missingNumber.cpp
+++ missingNumber.cpp
@@ -38,6 +38,25 @@ public:
     }
 };
 
+// Optimal Approach (Using XOR)
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums){
+        int n = nums.size();
+        int xorSum = 0;
+        for(int i=0;i<=n;i++){
+            xorSum ^= i;
+        }
+        for(int num : nums){
+            xorSum ^= num;
+        }
+        return xorSum;
+    }
+};
+
 int main()
 {
     int n;
