50514d755869d569d41bcae89c9122227ebbe0d0
Adarsh Srivastav <adarshsrivastav49455@gmail.com>
2025-12-21 22:50:52 +0530

    add comments

--- reverseWordsInAString.cpp
+++ reverseWordsInAString.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 // Time Complexity : O(n)
@@ -7,34 +7,53 @@ using namespace std;
 class Solution {
 public:
     string reverseWords(string s) {
-        string ans = "";
-        int i = s.size() - 1;
+        string ans = "";              // Stores the final reversed string
+        int i = s.size() - 1;         // Start from the end of the string
 
+        // Traverse the string from right to left
         while (i >= 0) {
-            // skip spaces
-            while (i >= 0 && s[i] == ' ') i--;
 
+            // Skip trailing or extra spaces
+            while (i >= 0 && s[i] == ' ') {
+                i--;
+            }
+
+            // If end of string is reached, exit loop
             if (i < 0) break;
 
-            int j = i;
+            int j = i;                // Mark the end of the current word
 
-            // find word start
-            while (j >= 0 && s[j] != ' ') j--;
+            // Move j to the start of the current word
+            while (j >= 0 && s[j] != ' ') {
+                j--;
+            }
 
-            if (!ans.empty()) ans += ' ';
+            // Add a space before adding a new word (except for first word)
+            if (!ans.empty()) {
+                ans += ' ';
+            }
+
+            // Extract the word and append to result
             ans += s.substr(j + 1, i - j);
 
+            // Move i to the character before the current word
             i = j - 1;
         }
 
-        return ans;
+        return ans;                   // Return the reversed words string
     }
 };
 
-int main()
-{
+int main() {
     string s;
-    cin >> s;
+
+    // Read entire input line including spaces
+    getline(cin, s);
+
     Solution obj;
+
+    // Print reversed words
     cout << obj.reverseWords(s);
+
+    return 0;                         // End of program
 }
