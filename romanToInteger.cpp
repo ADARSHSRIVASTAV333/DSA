8fa2b950d367d21d780a4f4cfc15c8595b99b997
Adarsh Srivastav <adarshsrivastav49455@gmail.com>
2026-01-06 22:50:45 +0530

    add right to left traversal

--- romanToInteger.cpp
+++ romanToInteger.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 // Time Complexity : O(n)
 // Space Complexity : O(1)
+// Approach : Hashing
 
 class Solution{
     public:
@@ -25,6 +26,39 @@ class Solution{
                 else{
                     res = res + mpp[s[i]];
                 }
+            }
+            return res;
+        }
+};
+
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Alternative Approach : Right to Left Traversal
+
+class Solution{
+    public:
+        int romanToInteger(string s){
+            int n = s.length();
+            unordered_map<char,int> mpp = {
+                {'I',1},
+                {'V',5},
+                {'X',10},
+                {'L',50},
+                {'C',100},
+                {'D',500},
+                {'M',1000}
+            };
+            int res = 0;
+            int prev = 0;
+            for(int i=n-1;i>=0;i--){
+                int curr = mpp[s[i]];
+                if(curr < prev){
+                    res = res - curr;
+                }
+                else{
+                    res = res + curr;
+                }
+                prev = curr;
             }
             return res;
         }
