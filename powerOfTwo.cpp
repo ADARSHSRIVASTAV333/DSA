0b89bf10c7ca3710445dbb45e7c8c57671612761
Adarsh Srivastav <adarshsrivastav49455@gmail.com>
2026-01-12 23:31:37 +0530

    add iterative division method

--- powerOfTwo.cpp
+++ powerOfTwo.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
 // Time Complexity : O(1)
 // Space Complexity : O(1)
 
@@ -24,6 +25,21 @@ public:
         if (n<=0) return false;
         if(__builtin_popcount(n) == 1) return true;
         else return false;
+    }
+};
+
+// Iterative Division Method
+// Time Complexity : O(logn)
+// Space Complexity : O(1)
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n){
+        if(n<=0) return false;
+        while(n%2 == 0){
+            n = n/2;
+        }
+        return n == 1;
     }
 };
 
