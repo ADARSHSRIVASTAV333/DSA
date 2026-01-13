d2897e381aa81ccbf72f4ad29117fc00bf7028a2
Adarsh Srivastav <adarshsrivastav49455@gmail.com>
2026-01-13 22:46:50 +0530

    add sum of two integers

---
+++ addTwoIntegers.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Time Complexity : O(1)
+// Space Complexity : O(1)
+
+class Solution {
+public:
+    int sum(int num1, int num2) {
+        return num1+num2;
+    }
+};
+
+int main()
+{
+    int a,b;
+    cin >> a >> b;
+    Solution obj;
+    cout << obj.sum(a,b);
+}
