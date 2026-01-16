e8c3647eb48407ade816629ff727dc9cacc39a1b
Adarsh Srivastav <adarshsrivastav49455@gmail.com>
2026-01-16 22:07:41 +0530

    add compact bit manipulation method

--- powerOfTwo.cpp
+++ powerOfTwo.cpp
@@ -17,6 +17,17 @@ class Solution{
         }
 };
 
+// Compact Bit Manipulation Method
+// Time Complexity : O(1)
+// Space Complexity : O(1)
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n){
+        return n>0 && (n & (n-1)) == 0;
+    }
+};
+
 // Using Built-in Function
 // Time Complexity : O(1)
 // Space Complexity : O(1)
