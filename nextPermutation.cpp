cb8f0b29f0c2cb9309e008eaa304f131f6c8d498
Adarsh Srivastav <adarshsrivastav49455@gmail.com>
2026-02-12 17:06:04 +0530

    add next permutation

---
+++ nextPermutation.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+
+class Solution{
+    public:
+        void nextPermutation(vector<int> &nums){
+            next_permutation(nums.begin(),nums.end());
+        }
+};
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for(auto &val : nums){
+        cin >> val;
+    }
+    Solution obj;
+    obj.nextPermutation(nums);
+    for(auto &val : nums){
+        cout << val << " ";
+    }
+}
