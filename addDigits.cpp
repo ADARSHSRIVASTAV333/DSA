994ff4b8e75e8f7c884b83ac7632e44d44257322
Adarsh Srivastav <adarshsrivastav49455@gmail.com>
2026-02-15 18:43:35 +0530

    add add digits problem

---
+++ addDigits.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Time Complexity : O(1)
+// Space Complexity : O(1)
+
+class Solution {
+public:
+    int addDigits(int num) {
+        if(num == 0) return 0;
+        return 1 + (num-1) % 9;
+    }
+};
+
+int main()
+{
+    int num;
+    cin >> num;
+    Solution obj;
+    cout << obj.addDigits(num);
+}
